34270db2e9e83dece7ac0ed8bd3f44e08aa8a99c
dieggger <dieggger@gmail.com>
2017-01-30 18:56:17 -0700

    Created new components to separate the responsibilities a little bit more

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,29 +2,11 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: "" }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange (event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course: course });
-  }
-
-  onClickSave() {
-    // most verbose way of dispatching an action
-    this.props.actions.createCourse(this.state.course);
-    //alert(`Saving ${this.state.course.title}`);
   }
 
   courseRow (course, index) {
@@ -32,21 +14,12 @@ class CoursesPage extends React.Component {
   }
 
   render () {
+    // a bit of destructuring here to simplify the code
+    const {courses} = this.props;
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title}
-        />
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave}
-        />
+        <CourseList courses={courses}/>
       </div>
     );
   }
