725f85ddff78cbac6fe327ec9f8d952d7380356b
dieggger <dieggger@gmail.com>
2017-01-30 19:25:13 -0700

    added another route to handle specific courses, modified header to show new option on navbar, added redux container component to manage courses

---
+++ src/components/course/ManageCoursePage.js
@@ -0,0 +1,35 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+
+class ManageCoursePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <h1>Manage course</h1>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+  //courses: PropTypes.array.isRequired,
+  //actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    state: state
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
